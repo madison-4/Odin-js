9a5eff52b5d842a92e7d7a80ffc2e91ba29f42b0
noxcodes <samingosi97@gmail.com>
2025-11-06 08:54:46 +0300

    first push for README

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+This repo follows Odin project's javascript course
+My ALX curriculum is om hold as of yet
+
+## variables
---
+++ commit.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+git add .
+
+echo "Please Enter a commit message"
+read mess
+
+git commit -m "$mess"
+
+git push
